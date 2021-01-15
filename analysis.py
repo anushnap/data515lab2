8efc23489cab84a31a22f538de562b37a01f8b0e
anushnap <anushnap@uw.edu>
2021-01-14 20:54:45 -0800

    uncommented code to allow for Spokane or Fremont data spec

--- analysis.py
+++ analysis.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import sys
 
-def download_data():
-#	if (dataset == 'fremont'):
-#		dataframe = pd.read_csv('https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD')
-#	elif (dataset == 'spokane
-	dataframe = pd.read_csv('https://data.seattle.gov/api/views/upms-nr8w/rows.csv?accessType=DOWNLOAD')
+def download_data(dataset):
+	if (dataset == 'fremont'):
+		dataframe = pd.read_csv('https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD')
+	elif (dataset == 'spokane
+		dataframe = pd.read_csv('https://data.seattle.gov/api/views/upms-nr8w/rows.csv?accessType=DOWNLOAD')
 	return dataframe
 
 if __name__ == "__main__":
-	data = download_data()
+	data = download_data(dataset)
 	print(data)
 
